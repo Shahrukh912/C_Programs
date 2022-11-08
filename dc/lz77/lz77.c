2bd1df9119f45c8b49c8dbb451cd433156c60977
Shahrukh912 <shahrukhsiddiqui366@gmail.com>
2022-11-08 13:48:57 +0530

    added lz77 file

---
+++ dc/lz77/lz77.c
@@ -0,0 +1,156 @@
+/*
+DC Practical:
+LZ77 Encoding and Decoding:
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+//Global variables
+int lindex=-1; //it keeps track of where was the last character inserted in look ahead buffer to see its full..
+FILE *fout;
+
+int wsize,la,sb; //wsize is window size, and la is lookahead, sb is a search buffer
+char *l,*s;//array for look ahead and search buffer.
+
+void display(){
+    int i=0;
+    for(i=0;i<sb;i++){
+        printf("%c ",s[i]);
+    }
+    printf(" | ");
+    for(i=0;i<=lindex;i++){
+        printf("%c ",l[i]);
+    }
+    //printf("\tlindex=%d ,la=%d, sb=%d",lindex,la,sb);
+}
+
+int insertInBuffer(char c){ //zero means success -1 means full
+    if(lindex<la-1){
+        lindex++;
+        l[lindex] = c;
+    }
+    else{    
+        return -1;
+    }
+    return 0;
+}
+
+int searchInSeachBuffer(char c,int offset){
+    int i,len=0;
+    // if(offset>0){
+    //     offset++;
+    // }
+    for(i=sb-1-offset;i>=0;i--){
+        if(c == s[i]){
+            return i;
+        }
+    }
+    return -1; //means not found
+}
+int lenFound(int j){
+    int i=0,len=0;
+    for(;j<sb;j++,i++){
+        if(s[j] != l[i]){
+            return len;
+        }
+        len++;
+    }
+    for(j=0;i<la;i++,j++){
+        if(l[j] != l[i]){
+            return len;
+        }
+        len++;
+    }
+    return len;
+}
+void dumpLeft(int len,int dumpInFile){
+    char c;
+    int i=0,j;
+    while(len>0){
+        c = s[0];
+        for(i=1;i<sb;i++){
+            s[i-1] = s[i];
+        }
+        s[i-1] = l[0] ;
+        for(i=1;i<sb;i++){
+            l[i-1] = l[i];
+        }
+        if(dumpInFile == 1){
+            fputc(c,fout);
+        }
+        len--;
+        lindex--;//so that there would be empty space for new values
+    }
+}
+void lz77Encode(){
+    int i=0,j=0,t=sb,tlen,offset=0,len=0,z=0; //negative minumn so that max value can be assign
+    //t is temporary index.
+    while(t!=-1){
+        t = searchInSeachBuffer(l[0],sb-t);
+        tlen = lenFound(t);
+        //printf("{t=%d ,tlen=%d}  ",t,tlen);
+        if(tlen > len){
+            len = tlen;
+            offset = sb-t; // calculating offset from actual returned index
+        }
+    }
+    fprintf(fout,"<%d,%d,c(%c)>\n",offset,len,l[len]);
+    printf("<%d,%d,c(%c)>\n",offset,len,l[len]);
+    dumpLeft(len+1,0);
+}
+int main(){
+    char st[100],charSet[26],in[100],out[100],w[20],t;
+    int m,i=0,temp;
+
+
+    //Inputs
+    printf("Enter input file name (press enter to use in.txt): ");
+    fgets(in,100,stdin);
+    if(strcmp(in,"")){
+        strcpy(in,"in.txt");
+    }
+    printf("Enter output file name (press enter to use out.txt): ");
+    fgets(out,100,stdin);
+    if(strcmp(out,"")){
+        strcpy(out,"out.txt");
+    }
+    printf("Enter buffer size : ");
+    scanf("%d",&la);
+    printf("Enter Look search buffer : ");
+    scanf("%d",&sb);
+    wsize = sb+la; //calculating search buffer.
+    printf("window size is %d\n",wsize);
+
+    l = (char *)calloc(la,sizeof(char)); //creating the array,
+    s = (char *)calloc(sb,sizeof(char)); //creating the array.
+
+
+    
+    FILE *fin = fopen(in,"r");
+    if(fin == NULL){
+        printf("Please create a input file name %s\n\n",in);
+    }
+    else{
+        fout = fopen(out,"w");
+        
+        while((t=fgetc(fin))!=EOF){
+
+           temp = insertInBuffer(t);
+           printf("[sc : %c]\n",t);
+           display();
+           printf("\n");
+            if(temp == -1){
+                lz77Encode();
+                insertInBuffer(t);
+                printf("\n-------------------------\n");
+            }    
+                 
+        }
+        
+    }
+    fclose(fin);
+    fclose(fout);
+    
+
+}
